8280ddba5d8a59530b3935bc356c674dd1c73cab
sk-ys <963792+sk-ys@users.noreply.github.com>
2020-01-22 20:31:31 +0900

    init.rb

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :external_relations do
+  name 'External Relations plugin'
+  author 'sk-ys'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/sk-ys/redmine_external_relations'
+  author_url 'https://github.com/sk-ys/'
+end
